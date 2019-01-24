94519622518ca8cdf1d6c9145c8d9f8fe6b32282
DarrenChangSD <43389229+DarrenChangSD@users.noreply.github.com>
2019-01-24 08:59:38 -0800

    initial commit of java explore

---
+++ threeSumSmaller.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class threeSumSmaller {
+
+    public static void main(String[] args) {
+
+      int[] nums = {-2, 0, 1, 3};
+      int target = 2;
+      System.out.println(tSumSmaller(nums, target));
+    }
+
+    public static int tSumSmaller(int[] nums, int target) {
+        //initialize total count to zero
+        int count = 0;
+
+        //sort the array
+        Arrays.sort(nums);
+
+        //loop through entire array
+        for(int i = 0; i < nums.length - 2; i++) {
+            //set left to i + 1
+            int left = i + 1;
+            //set right to end of array
+            int right = nums.length - 1;
+
+            //while left index < right index
+            while(left < right) {
+                //if the 3 indices add to less than the target increment count
+                if(nums[i] + nums[left] + nums[right] < target) {
+                    //increment the count by the distance between left and right because the array is sorted
+                    count += right - left;
+
+                    //increment left pointer
+                    left++;
+                } else {
+                    //if they sum to a value greater than target...
+                    //decrement right pointer
+                    right--;
+                }
+            }
+        }
+        return count;
+    }
+}
